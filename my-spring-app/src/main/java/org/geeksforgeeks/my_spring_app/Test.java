4139b449c0f15aef611d023b439109d0a9389009
Shantanu Shubham <shantanu0707@gmail.com>
2026-01-18 16:33:50 +0530

    Spring App

--- my-spring-app/src/main/java/org/geeksforgeeks/my_spring_app/Test.java
+++
@@ -1,8 +0,0 @@
-package org.geeksforgeeks.my_spring_app;
-
-public class Test {
-
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
-}
---
+++ my-spring-app/src/main/java/org/geeksforgeeks/my_spring_app/controller/MathController.java
@@ -0,0 +1,19 @@
+package org.geeksforgeeks.my_spring_app.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MathController {
+
+    @GetMapping("/add")
+    public int add(@RequestParam int a, @RequestParam int b) {
+        return a + b;
+    }
+
+    @GetMapping("/subtract")
+    public int subtract(@RequestParam int a, @RequestParam int b) {
+        return a - b;
+    }
+}
